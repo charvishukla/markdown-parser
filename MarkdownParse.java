853e62b8fb9e4820306a957c9fadda9e8c287606
Charvi Shukla <67646308+charvishukla@users.noreply.github.com>
2022-05-08 05:28:58 -0700

    changed variable name

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class MarkdownParse {
 public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
+        ArrayList<String> res = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         if(markdown.length() == 0){
@@ -41,13 +41,13 @@ public static ArrayList<String> getLinks(String markdown) {
                     break;
                 }
 
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            res.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
 
         }
         
 
-        return toReturn;
+        return res;
     }
  
 
